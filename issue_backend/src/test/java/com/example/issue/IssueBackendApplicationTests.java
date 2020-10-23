39668841315981f2aab137d88a08eda71954290f
joshua <1037261334@qq.com>
2020-10-23 17:01:46 +0800

    Add Bakend for ISSUE

---
+++ issue_backend/src/test/java/com/example/issue/IssueBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.issue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

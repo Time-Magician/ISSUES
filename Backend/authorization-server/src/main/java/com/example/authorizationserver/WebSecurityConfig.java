2cb5fdc98cfeb7f35461a7c3912268035f8449c9
SJTU_ZYC <2824058153@qq.com>
2020-11-13 18:25:46 +0800

    feat: 添加了OAuth配置

---
+++ Backend/authorization-server/src/main/java/com/example/authorizationserver/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.example.authorizationserver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+}
